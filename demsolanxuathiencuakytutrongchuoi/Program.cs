81f14be77ba37ca1ddcf8bb85c398b0fd964ef62
kienb <kienb@KIN>
2023-12-29 20:02:00 +0700

    Add project files.

---
+++ demsolanxuathiencuakytutrongchuoi/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "hello, work";
+
+        Console.Write("nhap mot ky tu:");
+
+        char dem = Console.ReadLine()[0];
+
+
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == dem)
+            {
+
+                count++;
+
+
+
+            }
+
+        }
+        Console.WriteLine($"Số lần xuất hiện của ký tự '{dem}' trong chuỗi là: {count}");
+
+    }
+}
